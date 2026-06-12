2ce3de772dbf2bd21818ed5606b69afa233faf41
MatteousCastaneda <matteous.castaneda@g.austincc.edu>
2026-06-12 14:41:10 -0500

    Edited follow and playercontroller scripts
    moved the camera to use late update to fix stutter. Moved the move forward to regular update in the playercontroller

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -14,14 +14,16 @@ public class PlayerControllerX : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
-
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
