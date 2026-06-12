fa7af58304a20f56c1aa47a98da71fbe5bc25999
MatteousCastaneda <matteous.castaneda@g.austincc.edu>
2026-06-12 14:33:20 -0500

    Changed camera offset

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(30, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-
+      
     }
 
     // Update is called once per frame
